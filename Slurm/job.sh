178c13400b66e58073793b467356ecf7fb382b34
Zoatik <119761787+Zoatik@users.noreply.github.com>
2026-09-23 22:54:56 +0200

    chore: removed helloworld file + 1 cpu requested for slurm

--- Slurm/job.sh
+++ Slurm/job.sh
@@ -5,12 +5,14 @@
 #SBATCH --partition=cpu
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=1
 #SBATCH --mem=64M
 #SBATCH --time=01:00:00
 #SBATCH --output=/data/results/output_%j.out
 
+set -e
+
 # 3. Run your program
 echo "Job started on $(date)"
-sh count_to_1M
+bash count_to_1M
 echo "Job finished on $(date)"
