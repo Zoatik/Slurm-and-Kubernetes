666fa380a68cec9ae339cf49cf8ec584ead0da45
Zoatik <119761787+Zoatik@users.noreply.github.com>
2026-09-23 17:53:58 +0200

    fix(job.sh): select a valid partition + resize mem. to 64M

--- Slurm/job.sh
+++ Slurm/job.sh
@@ -2,14 +2,13 @@
 #SBATCH --job-name=my_test_job
 #SBATCH --output=output_%j.log
 #SBATCH --error=error_%j.log
-#SBATCH --partition=normal
+#SBATCH --partition=cpu
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=8G
+#SBATCH --mem=64M
 #SBATCH --time=01:00:00
-
-cd ../scripts
+#SBATCH --output=/data/results/output_%j.out
 
 # 3. Run your program
 echo "Job started on $(date)"
